064d2b857f055b5f7b02cc7253958610b849fbce
Carlos Daniel <carlosdngfa@gmail.com>
2023-12-15 17:45:28 -0300

    Project Created

---
+++ recep10/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import style from '../css/Navbar.module.css'
+
+function Navbar(){
+    return(
+        <div className={style.content}>
+            <h1>Magic</h1>
+        </div>
+    )
+}
+
+export default Navbar
