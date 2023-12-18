ad5f4e1de626f95e12592d3b98e44701d9d1821e
Carlos Daniel <carlosdngfa@gmail.com>
2023-12-18 15:15:59 -0300

    Home page finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,21 @@
 import style from '../css/Navbar.module.css'
-import { abrirDialog } from '../functions/DialogController'
+import { abrirDialog, fecharDialog } from '../functions/DialogController'
+import exit from '../images/exit.png'
 
 function Navbar(){
     return(
         <div className={style.content}>
             <button onClick={abrirDialog}>ENTRAR</button>
             <dialog>
+                <img onClick={fecharDialog} className={style.close} src={exit} alt="sair"></img>
                 <div className={style.form}>
-                    <h1>TESTE</h1>
+                    <h1>ENTRAR</h1>
+                </div>
+                <div className={style.login}>
+                    <input type="text" name="campo" id="campologin" placeholder="Digite seu email aqui"></input>
+                </div>
+                <div className={style.login}>
+                    <input type="text" name="senha" id="camposenha" placeholder="Digite aqui sua senha"></input>
                 </div>
             </dialog>
         </div>
--- src/pages/Interface.js
+++ src/pages/Interface.js
@@ -1,15 +1,24 @@
 import style from "../css/Interface.module.css"
 import Navbar from "../components/Navbar"
 import logo from "../images/logo.png"
+import background from "../images/church.jpg"
 
 function Interface(){
     return(
         <div className={style.content}>
-            <Navbar/>
-            <div className={style.geral}>
-                <h1>Seja Bem-Vindo(a)</h1>
-            </div>
-            <img src={logo} alt="logo"></img>
+            <img className={style.backg} src={background} alt="background"></img>
+                <Navbar/>
+                <p className={style.nomeigreja}>Adventista do 7º Dia</p>
+                <div className={style.geral}>
+                    <h1>Ame a Deus,<br></br> ame servir.</h1>
+                </div>
+                <div>
+                    <ul className={style.bar}>
+                        <li>COMUNIDADE</li>
+                        <li>SOBRE NÓS</li>
+                    </ul>
+                </div>
+                <img className={style.logoinicio} src={logo} alt="logo"></img>
         </div>
     )
 }
