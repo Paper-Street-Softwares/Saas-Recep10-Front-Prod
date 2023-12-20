f93c420c5af498b7a96c1672d3432ee052f17477
Carlos Daniel <carlosdngfa@gmail.com>
2023-12-19 23:15:22 -0300

    Add method POST

--- src/functions/DialogController2.js
+++ src/functions/DialogController2.js
@@ -1,11 +1,54 @@
 function abrirDialog2() {
-    const dialog = document.getElementById('dialog2');
-    dialog.showModal();
-  }
-  
-  function fecharDialog2() {
-    const dialog = document.getElementById('dialog2');
-    dialog.close();
-  }
-  
-  export {abrirDialog2, fecharDialog2}
+  const dialog = document.getElementById('dialog2');
+  dialog.showModal();
+}
+
+function fecharDialog2() {
+  const dialog = document.getElementById('dialog2');
+  dialog.close();
+}
+
+function enviarVisitante() {
+
+  const form = document.getElementById('form');
+
+  form.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const data = new FormData(form);
+
+    // Construa o objeto de dados com os valores de todos os campos
+    const visitante = {
+      nome: data.get('nome'),
+      telefone: data.get('telefone'),
+      genero: data.get('genero'),
+      idade: data.get('idade'),
+      endereco: data.get('endereco'),
+      cidadeestado: data.get('cidadeestado'),
+      religiao: data.get('religiao'),
+      grupo: data.get('grupo'),
+      estudo: data.get('estudo'),
+    };
+
+    // Envie o objeto de dados para o servidor
+    try {
+      const res = await fetch(
+        'https://recep10-back.up.railway.app/api/visitantes',
+        {
+          method: 'POST',
+          body: data,
+        },
+      );
+
+      // Se a solicitação for bem-sucedida, imprima uma mensagem de sucesso
+      if (res.status === 201) {
+        console.log('Visitante registrado com sucesso!');
+      }
+    } catch (err) {
+      // Se a solicitação falhar, imprima uma mensagem de erro
+      console.log(err.message);
+    }
+  });
+}
+
+export {abrirDialog2, fecharDialog2, enviarVisitante}
--- src/pages/Painel.js
+++ src/pages/Painel.js
@@ -1,5 +1,5 @@
 import style from '../css/Painel.module.css'
-import { abrirDialog2, fecharDialog2 } from '../functions/DialogController2'
+import { abrirDialog2, enviarVisitante, fecharDialog2 } from '../functions/DialogController2'
 import { abrirDialog3, fecharDialog3 } from '../functions/DialogController3'
 
 function Painel(){
@@ -13,40 +13,33 @@ function Painel(){
                     <li>BUSCAR VISITAS</li>
                 </ul>
                 <dialog className={style.register} id="dialog2">
-                    <form className={style.formulario}>
-                    <h1>Cadastro de Visitante</h1>
-                        <label>
-                            <div><br></br>
-                                <input type='text' placeholder="Nome completo"></input>
-                            </div>
-                            <div><br></br>
-                                <input type='text' placeholder="Telefone"></input>
-                            </div>
-                            <div><br></br>
-                                <select defaultValue="">
-                                    <option value="" disabled>Gênero</option>
-                                    <option>Masculino</option>
-                                    <option>Feminino</option>
-                                </select>
-                            </div>
-                            <div><br></br>
-                                <input type='text' placeholder="Cidade e Estado"></input>
-                            </div>
-                            <div><br></br>
-                                <input type='text' placeholder="Religião"></input>
-                            </div>
-                            <div><br></br>
-                                <input type='text' placeholder="Frequenta pequeno grupo? Se sim, qual?"></input>
-                            </div>
-                            <div><br></br>
-                                <input type='text' placeholder="Faz estudo bíblico? Se sim, com quem?"></input>
-                            </div>
-                        </label>
+                    <form id="form" className={style.formulario}>
+                        <h1>Cadastro de Visitante</h1>
+                            <div>
+                                <input maxLength={53} id="nome" type='text' placeholder="Nome completo"></input>
+                                <br></br><br></br>
+                                <input maxLength={17} id="telefone" type='tel' placeholder="Telefone"></input>
+                                <br></br><br></br>
+                                <select id="genero" defaultValue=""><option value="" disabled>Gênero</option><option>Masculino</option><option>Feminino</option></select>
+                                <br></br><br></br>
+                                <input maxLength={3} id="idade" type='age' placeholder="Idade"></input>
+                                <br></br><br></br>
+                                <input maxLength={50} id="endereco" type='text' placeholder="Endereço"></input>
+                                <br></br><br></br>
+                                <input maxLength={50} id="cidadeestado" type='text' placeholder="Cidade e Estado"></input>
+                                <br></br><br></br>
+                                <input maxLength={20} id="religiao" type='text' placeholder="Religião"></input>
+                                <br></br><br></br>
+                                <input maxLength={53} id="grupo" type='text' placeholder="Frequenta pequeno grupo? Se sim, qual?"></input>
+                                <br></br><br></br>
+                                <input maxLength={53} id="estudo" type='text' placeholder="Faz estudo bíblico? Se sim, com quem?"></input>
+                        </div>
+                        <div className={style.btns}>
+                            <button onClick={fecharDialog2} className={style.btnback}>VOLTAR</button>
+                            <button onClick={enviarVisitante} type="submit" className={style.btnregister}>CADASTRAR</button>
+                        </div>
                     </form>
-                    <div className={style.btns}>
-                        <button onClick={fecharDialog2} className={style.btnback}>VOLTAR</button>
-                        <button className={style.btnregister}>CADASTRAR</button>
-                    </div>
+
                 </dialog>
 
                 {/* Acima abre o REGISTRO DE VISITANTES e Abaixo ADICIONA VISITAS aos visitantes */}
@@ -54,17 +47,14 @@ function Painel(){
                 <dialog className={style.register2} id="dialog3">
                     <form className={style.formulario2}>
                     <h1>Adicionar Visita</h1>
-                        <label>
-                            <div><br></br>
-                                <input className={style.data} type='text' placeholder="Data"></input>
-                            </div>
-                            <div><br></br>
-                                <select defaultValue="">
-                                    <option value="" disabled>Visitantes</option>
-                                    <option>Carlos Daniel</option>
-                                </select>
-                            </div>
-                        </label>
+                        <div>
+                            <input className={style.data} type='text' placeholder="Data"></input>
+                            <br></br><br></br>
+                            <select defaultValue="">
+                                <option value="" disabled>Visitantes</option>
+                                <option>Carlos Daniel</option>
+                            </select>
+                        </div>
                     </form>
                     <div className={style.btns}>
                         <button onClick={fecharDialog3} className={style.btnback}>VOLTAR</button>
