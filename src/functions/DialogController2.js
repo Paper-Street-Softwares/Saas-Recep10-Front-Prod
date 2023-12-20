75f4a91f8910aaf651b5c87a1bc4a7064f9e1497
Edison Matos <edison.matos@live.com>
2023-12-20 09:06:40 -0300

    fix(register): Fix POST method

--- src/functions/DialogController2.js
+++ src/functions/DialogController2.js
@@ -1,48 +1,76 @@
 function abrirDialog2() {
-  const dialog = document.getElementById('dialog2');
+  const dialog = document.getElementById("dialog2");
   dialog.showModal();
 }
 
 function fecharDialog2() {
-  const dialog = document.getElementById('dialog2');
+  const dialog = document.getElementById("dialog2");
   dialog.close();
 }
 
+const inputName = document.getElementById();
+
 function enviarVisitante() {
+  const form = document.getElementById("form");
 
-  const form = document.getElementById('form');
-
-  form.addEventListener('submit', async event => {
+  form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
     const data = new FormData(form);
 
     // Construa o objeto de dados com os valores de todos os campos
     const visitante = {
-      nome: data.get('nome'),
-      telefone: data.get('telefone'),
-      genero: data.get('genero'),
-      idade: data.get('idade'),
-      endereco: data.get('endereco'),
-      cidadeestado: data.get('cidadeestado'),
-      religiao: data.get('religiao'),
-      grupo: data.get('grupo'),
-      estudo: data.get('estudo'),
+      nome: data.get("nome"),
+      telefone: data.get("telefone"),
+      genero: data.get("genero"),
+      idade: data.get("idade"),
+      endereco: data.get("endereco"),
+      cidadeestado: data.get("cidadeestado"),
+      religiao: data.get("religiao"),
+      grupo: data.get("grupo"),
+      estudo: data.get("estudo"),
     };
+
+    console.log(data);
+
+    const registerTest = {
+      name: "aloaloooalo",
+      phone: "5561992781077",
+      gender: "Masculino",
+      age: 30,
+      address: "Endereço",
+      cityAndState: "São Paulo - SP",
+      religion: "Adventista",
+      smallGroup: "Não frequenta",
+      bibleStudy: "Sim, com amigo",
+    };
+
+    console.log(registerTest);
 
     // Envie o objeto de dados para o servidor
     try {
+      //       const res = await fetch(
+      //   "https://recep10-back.up.railway.app/api/visitantes",
+      //   {
+      //     method: "POST",
+      //     body: data,
+      //   }
+      // );
+
       const res = await fetch(
-        'https://recep10-back.up.railway.app/api/visitantes',
+        "https://recep10-back.up.railway.app/api/visitantes",
         {
-          method: 'POST',
-          body: data,
-        },
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(registerTest),
+        }
       );
 
       // Se a solicitação for bem-sucedida, imprima uma mensagem de sucesso
       if (res.status === 201) {
-        console.log('Visitante registrado com sucesso!');
+        console.log("Visitante registrado com sucesso!");
       }
     } catch (err) {
       // Se a solicitação falhar, imprima uma mensagem de erro
@@ -51,4 +79,4 @@ function enviarVisitante() {
   });
 }
 
-export {abrirDialog2, fecharDialog2, enviarVisitante}
+export { abrirDialog2, fecharDialog2, enviarVisitante };
