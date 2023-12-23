52f13f76185ddb168e6fab1f6b4e289b08fb1074
GabrielAdans <adans.gabriel@gmail.com>
2023-12-23 10:38:52 -0300

    feat(respon):styling forms

--- src/pages/Painel.js
+++ src/pages/Painel.js
@@ -14,7 +14,11 @@ import visitorsmg from '../images/visitors.png';
 import training from '../images/training.png';
 import anime from "animejs";
 import { updateUser } from '../functions/updateUser';
-
+import { FaUserPlus, FaUser, FaIdBadge, FaBell, FaEnvelope, FaSettings } from 'react-icons/fa';
+import { CiCalendarDate, CiSearch } from "react-icons/ci";
+import { TbReport } from "react-icons/tb";
+import { IoIosMore } from "react-icons/io";
+import { PiUserPlusThin } from "react-icons/pi";
 
 function Painel(){
   
@@ -149,7 +153,7 @@ axios.get(`https://recep10-back.up.railway.app/api/visitantes/${itemId}`)
         console.log(visitor); // Isso será executado sempre que visitor mudar
       }, [visitor]);
 
-    return(        
+    return(     
         <div className={style.content}>
             <div className={style.dashboard}>
                     <div className={style.card}>
@@ -178,22 +182,14 @@ axios.get(`https://recep10-back.up.railway.app/api/visitantes/${itemId}`)
                     <form id="form" className={style.formulario}>
                         <h1>Cadastro de Visitante</h1>
                             <div>
-                                <input maxLength={53} id="nome" type='text' placeholder="Nome completo"></input>
-                                <br></br><br></br>
+                                <input maxLength={53} id="nome" type='text' placeholder="Nome"></input>
                                 <input maxLength={17} id="telefone" type='text' pattern="[0-9]{9,17}" placeholder="Telefone"></input>
-                                <br></br><br></br>
                                 <select id="genero" defaultValue=""><option value="" disabled>Gênero</option><option>Masculino</option><option>Feminino</option></select>
-                                <br></br><br></br>
                                 <input id="idade" type="text" maxLength={3} pattern="[0-9]{1,3}" placeholder="Idade"/>
-                                <br></br><br></br>
                                 <input maxLength={50} id="endereco" type='text' placeholder="Endereço"></input>
-                                <br></br><br></br>
                                 <input maxLength={50} id="cidadeestado" type='text' placeholder="Cidade e Estado"></input>
-                                <br></br><br></br>
                                 <input maxLength={20} id="religiao" type='text' placeholder="Religião"></input>
-                                <br></br><br></br>
                                 <input maxLength={53} id="grupo" type='text' placeholder="Frequenta pequeno grupo? Se sim, qual?"></input>
-                                <br></br><br></br>
                                 <input maxLength={53} id="estudo" type='text' placeholder="Faz estudo bíblico? Se sim, com quem?"></input>
                         </div>
                         <div className={style.btns}>
@@ -262,6 +258,24 @@ axios.get(`https://recep10-back.up.railway.app/api/visitantes/${itemId}`)
                 <h2>
                   Hoje é {formatarData(dataHoraAtual)} - {formatarHora(dataHoraAtual)}
                 </h2>
+              </div>
+              <div className={style.navbar}>
+                <div className={style.navbarborder} />
+                  <div onClick={abrirDialog} className={style.icon}>
+                    <PiUserPlusThin />
+                  </div>
+                  <div onClick={abrirDialog3} className={style.icon}>
+                    <CiCalendarDate />
+                  </div>
+                  <div onClick={abrirDialog4} className={style.icon}>
+                    <CiSearch />
+                  </div> 
+                  {/*<div className={style.icon}>
+                    <TbReport />
+                  </div> */}
+                  <div className={style.icon}>
+                    <IoIosMore />
+                  </div>                 
               </div>
             </div>
     );
