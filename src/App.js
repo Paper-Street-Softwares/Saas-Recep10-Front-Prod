24ff94eb8b809d7e38bcff8052f4068b92ffb5d5
Edison Matos <edison.matos@live.com>
2023-12-26 10:00:00 -0300

    feat(login): Upload app.js updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import './App.css';
-import Interface from './pages/Interface';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Painel from './pages/Painel';
-import { PrivateRoute } from './pages/privateRoute';
+import "./App.css";
+import Interface from "./pages/Interface";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Painel from "./pages/Painel";
+import { PrivateRoute } from "./pages/privateRoute";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-        <Router>
-          <Routes>
-              <Route path="/painel" element={<PrivateRoute><Painel/></PrivateRoute>} />
-              <Route path="/home" element={<Interface/>} />
-          </Routes>
+      <Router>
+        <Routes>
+          <Route
+            path="/painel"
+            element={
+              <PrivateRoute>
+                <Painel />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/home" element={<Interface />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
       </Router>
     </div>
   );
