f5b7304d253f5932f3a113bc7ccb6b2cae9cdbf5
Carlos Daniel <carlosdngfa@gmail.com>
2023-12-26 09:19:59 -0300

    Private Routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Interface from './pages/Interface';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Painel from './pages/Painel';
+import { PrivateRoute } from './pages/privateRoute';
 
 function App() {
   return (
     <div className="App">
         <Router>
           <Routes>
-              <Route path="/painel" element={<Painel/>} />
+              <Route path="/painel" element={<PrivateRoute><Painel/></PrivateRoute>} />
               <Route path="/home" element={<Interface/>} />
           </Routes>
       </Router>
