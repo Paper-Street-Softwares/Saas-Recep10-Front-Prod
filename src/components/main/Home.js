0c84566e2eb164f05a118123f1c74587c70b32e6
Edison Matos <edison.matos@live.com>
2023-12-31 07:26:34 -0300

    feat(home): Add content on Home

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,9 +1,20 @@
 function Home() {
   return (
     <div>
-      <h1 className="giantTitle-Global">
-        QUAL QUE SERIA O ROLÊ EXISTENTE AQUI?
-      </h1>
+      <img src="https://recepcao10.up.railway.app/assets/images/about/about-image.svg" />
+      <p className="titles-Global">
+        Recepção Nota 10 - Agora seus visitantes se tornarão membros ativos!
+      </p>
+      <p className="texts-Global">Alguma coisa curta</p>
+      <p className="titles-Global">Suporte</p>
+      <p className="texts-Global">
+        Caso precisa entrar em contato conosco para reportar alguma dificuldade
+        no uso do sistema ou para qualquer tipo de ajuda, clique aqui.
+      </p>
+      <p className="titles-Global">Como tratamos os dados armazenados aqui?</p>
+      <p className="texts-Global">
+        Política de privacidade | Termos de uso | LGPD
+      </p>
     </div>
   );
 }
