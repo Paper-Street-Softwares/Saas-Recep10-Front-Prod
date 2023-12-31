e71dd71dc25367bf43b9d9df8953c3351b65f3d9
GabrielAdans <adans.gabriel@gmail.com>
2023-12-31 01:53:51 -0300

    feat(header):real time date add

--- src/components/structure/Header.js
+++ src/components/structure/Header.js
@@ -1,15 +1,43 @@
+import React, { useState, useEffect } from "react";
 import style from "../../css/structure/header.css";
 import profilePic from "../../images/profilePic.png";
 
 const Header = () => {
+  const [dataAtual, setDataAtual] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDataAtual(new Date());
+    }, 1000);
+
+    // Limpa o intervalo quando o componente é desmontado
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formatarData = (data) => {
+    const options = {
+      day: "numeric",
+      month: "long",
+    };
+
+    return new Intl.DateTimeFormat("pt-BR", options).format(data);
+  };
+
   return (
     <div className="header-Header">
-      <div className="profilePic-Header" id="profilePic-Header">
-        <img id="profilePic-Header" src={profilePic} />
+      <div className="headerGroup">
+        <div className="headerLeft-Header">
+          <img id="profilePic-Header" src={profilePic} alt="Profile Pic" />
+        </div>
+        <div className="textHeader-Header">
+          <div className="welcomeMesage-Header">Bem vindo,</div>
+          <div className="userName-Header">Edilson Matos</div>
+        </div>
       </div>
-      <div className="textHeader-Header">
-        <div className="welcomeMesage-Header">Bem vindo,</div>
-        <div className="userName-Header">Edilson Matos</div>
+
+      <div className="headerRigh-Header">
+        <div className="welcomeMesage-Header">Hoje é dia,</div>
+        <div className="realTimeHeader-Header">{formatarData(dataAtual)}</div>
       </div>
     </div>
   );
